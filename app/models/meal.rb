69d33bab92e61761ebf5c6edffd4c82f67ea8579
Benjamin West <ben@quickleft.com>
2013-11-18 23:53:17 -0700

    Added enumeration to mealtimes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,6 +6,8 @@ class Meal < ActiveRecord::Base
 
   validate :valid_day
 
+  MEALTIMES = %W(breakfast second_breakfast brunch elevensies lunch sunday_dinner sunday_desert afternoon_tea supper dinner bachelors_dinner)
+
   def valid_day
     errors.add(:day, "Date must not be in the past") if day < Time.now.to_date
   end
