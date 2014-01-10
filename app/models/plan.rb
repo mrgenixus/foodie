2cfe3366761a838c2f4703acf7bb2719a7606d77
Benjamin West <mrgenixus@gmail.com>
2014-01-10 13:27:56 -0700

    Make this an ActiveModel::Model so that I can get away from all the meaningless methods

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,24 +1,7 @@
 class Plan
 
+  include ActiveModel::Model
+  
   attr_accessor :meals
-
-  def self.Name
-    "Plan"
-  end
-
-  def self.model_name
-    return ActiveModel::Name.new(Plan)
-  end
-
-  def to_key
-    [0]
-  end
-
-  def persisted?
-    false
-  end
-
-  def id
-  end
 
 end
